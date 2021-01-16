58ca544938e74e2fb1bd839baaaa51538134265c
Patrick Jahn <Patrick.jackobsen.jahn.109@gmail.com>
2021-01-16 22:18:36 +0100

    Setup

---
+++ src/main/java/facades/RemoteServerFacade.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facades;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.characterDTO;
+import dto.filmDTO;
+import dto.combinedDTO;
+import dto.planetDTO;
+import errorhandling.API_Exception;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import utils.HttpUtils;
+
+/**
+ *
+ * @author Patrick
+ */
+public class RemoteServerFacade {
+    
+      private static EntityManagerFactory emf;
+      private static RemoteServerFacade instance;
+      private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    
+   
+    public RemoteServerFacade(){}
+      /**
+     *
+     * @param _emf
+     * @return the instance of this facade.
+     */
+    public static RemoteServerFacade getRemoteServerFacade(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new RemoteServerFacade();
+        }
+        return instance;
+    }
+    
+    
+   
+}
---
+++ src/main/java/rest/DemoResource.java
@@ -0,0 +1,89 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import entities.User;
+import errorhandling.API_Exception;
+import facades.RemoteServerFacade;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import javax.annotation.security.RolesAllowed;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import utils.EMF_Creator;
+
+/**
+ * @author Patrick
+ */
+@Path("info")
+public class DemoResource {
+    
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+   private static final RemoteServerFacade remoteFACADE =  RemoteServerFacade.getRemoteServerFacade(EMF);
+    
+    @Context
+    private UriInfo context;
+
+    @Context
+    SecurityContext securityContext;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String  getInfoForAll() {
+        return  "{\"msg\":\"Hello anonymous\"}";
+   
+    }
+
+    //Just to verify if the database is setup
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("all")
+    public String allUsers() {
+
+        EntityManager em = EMF.createEntityManager();
+        try {
+            TypedQuery<User> query = em.createQuery ("select u from User u", User.class);
+            List<User> users = query.getResultList();
+            return "\"count\" : \"" + users.size() + "\"";
+        } finally {
+            em.close();
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("user")
+    @RolesAllowed("user")
+    public String getFromUser() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Hello to User: " + thisuser + "\"}";
+   
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("admin")
+    @RolesAllowed("admin")
+    public String getFromAdmin() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+          return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
+    }
+    
+    
+    
+}   
